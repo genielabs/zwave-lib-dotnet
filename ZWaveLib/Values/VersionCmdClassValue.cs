a965e559244ae2da950ded71f6af4758b9d5be95
mdave <dave@xyloid.org>
2015-08-23 18:39:44 +0000

    Update headers

--- ZWaveLib/Values/VersionCmdClassValue.cs
+++ ZWaveLib/Values/VersionCmdClassValue.cs
@@ -1,18 +1,18 @@
 ﻿/*
-    This file is part of HomeGenie Project source code.
+    This file is part of ZWaveLib Project source code.
 
-    HomeGenie is free software: you can redistribute it and/or modify
+    ZWaveLib is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    HomeGenie is distributed in the hope that it will be useful,
+    ZWaveLib is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with HomeGenie.  If not, see <http://www.gnu.org/licenses/>.  
+    along with ZWaveLib.  If not, see <http://www.gnu.org/licenses/>.  
 */
 
 /*
